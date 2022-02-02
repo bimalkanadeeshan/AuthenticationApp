f4b96cbac47da3a6984371602bccc8d95c868b16
bimalkanadeeshan <mwbnadee@GMAIL.COM>
2022-02-02 09:19:33 +0530

    authentication business logic created

--- app/src/main/java/com/example/authenticationapp/MainActivity.java
+++ app/src/main/java/com/example/authenticationapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.authenticationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut(); //logout user
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        finish();
     }
 }
