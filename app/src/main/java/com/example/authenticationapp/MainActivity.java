b8395d614c0c0454ec428701160d1dd1a4602684
bimalkanadeeshan <mwbnadee@GMAIL.COM>
2022-02-02 16:21:49 +0530

    data retrieve from firestore

--- app/src/main/java/com/example/authenticationapp/MainActivity.java
+++ app/src/main/java/com/example/authenticationapp/MainActivity.java
@@ -1,25 +1,68 @@
 package com.example.authenticationapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView fullName,email,phone;
+    TextView mLogoutBtn;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        phone    = findViewById(R.id.profilePhone);
+        fullName = findViewById(R.id.profileName);
+        email    = findViewById(R.id.profileEmail);
+
+        mLogoutBtn  = findViewById(R.id.button);
+
+        fAuth  = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                phone.setText(value.getString("phone"));
+                fullName.setText(value.getString("fName"));
+                email.setText(value.getString("email"));
+            }
+        });
+
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.signOut(); //logout user
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
     }
 
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut(); //logout user
-        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-        finish();
-    }
+//    public void logout(View view) {
+//        FirebaseAuth.getInstance().signOut(); //logout user
+//        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+//        finish();
+//    }
 }
